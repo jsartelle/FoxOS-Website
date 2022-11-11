0d929acd0def9bec8c6fce7377bf1d073d7d323a
James Sartelle <james.sartelle@extra.app>
2022-11-11 15:45:41 -0600

    Markdown loading with Nuxt content

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,6 +8,7 @@ export default defineNuxtConfig({
   experimental: {
     reactivityTransform: true,
   },
+  modules: ['@nuxt/content'],
   routeRules: {
     '/': { redirect: '/desktop' },
     '/desktop': { ssr: false },
---
+++ pages/content/[...slug].vue
@@ -0,0 +1,11 @@
+<template>
+  <ContentDoc :path="path" />
+</template>
+
+<script setup lang="ts">
+import { useRoute } from 'vue-router'
+const route = useRoute()
+const path = $ref(
+  route.path.replace('/content', '').replaceAll('%20', '-').toLowerCase()
+)
+</script>
